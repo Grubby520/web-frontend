41caef1191c688e2bed2b3a4c8106efc53416ef1
jiangliangliang <jiangliangliang@starlinke.com>
2021-09-15 14:30:10 +0800

    update

--- Framework/Vue/key-points/vue.config.js
+++ Framework/Vue/key-points/vue.config.js
@@ -8,6 +8,7 @@ module.exports = {
         "@src": path.resolve(__dirname, "src"),
         "@components": path.resolve(__dirname, "src/components"),
         "@share": path.resolve(__dirname, "src/share"),
+        "@element": path.resolve(__dirname, "src/views/_element"),
         // "vue$": 'vue/dist/vue.esm.js', // 完整版
       },
     },
